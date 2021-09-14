713015a3883da3ba6375db4a6cf283ea811a1ab4
Vince <iwyswx@163.com>
2021-09-14 11:12:54 +0800

    update

--- pkg/models/versatel/v1alpha1/linstor/node.go
+++ pkg/models/versatel/v1alpha1/linstor/node.go
@@ -1,1 +1,39 @@
 package linstor
+
+
+func GetNodeData(ctx context.Context, c *client.Client) []map[string]string{
+	nodes,err := c.Nodes.GetAll(ctx)
+	resources, err := c.Resources.GetResourceView(ctx)
+	nodesInfo := []map[string]string{}
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, node := range nodes {
+		resNum := 0
+		for _, res := range resources{
+			if res.NodeName == node.Name{
+				resNum ++
+			}
+		}
+		sps,err := c.Nodes.GetStoragePools(ctx,node.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defaultInterface := node.NetInterfaces[0]
+		addr := fmt.Sprintf("%s:%d (%s)",defaultInterface.Address,defaultInterface.SatellitePort,defaultInterface.SatelliteEncryptionType)
+
+		nodeInfo := map[string]string{
+			"name":node.Name,
+			"nodeType":node.Type,
+			"resourceNum": fmt.Sprintf("%d",resNum),
+			"storagePoolNum":fmt.Sprintf("%d",len(sps)),
+			"addr": addr,
+			"status":node.ConnectionStatus,
+		}
+		nodesInfo = append(nodesInfo, nodeInfo)
+	}
+	return nodesInfo
+}
+
+
+func CreateNode()
