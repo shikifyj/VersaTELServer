b56bfe90ed3bc5fa960bf87f73deb9cd3d613414
shiki <237303802@qq.com>
2023-10-24 19:25:33 +0800

    修复部分浏览器不能正确显示时间的bug

--- pkg/models/versatel/v1alpha1/linstor/snapshot.go
+++ pkg/models/versatel/v1alpha1/linstor/snapshot.go
@@ -58,7 +58,7 @@ func GetSnapshot(ctx context.Context, c *client.Client) []map[string]string {
 						"resource": res.ResourceName,
 						"node":     []string{snapshot.NodeName},
 						"state":    res.Flags[0],
-						"time":     snapshot.CreateTimestamp.Time.String(),
+						"time":     snapshot.CreateTimestamp.Time.UTC().Format("2006-01-02T15:04:05Z07:00"),
 					}
 
 					resInfoMap[snapshot.SnapshotName] = resInfo
